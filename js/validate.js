d2711cfc52774d566b7aee23e9c191db3776791a
dambdb <baluev.aleksand@gmail.com>
2017-03-09 17:07:38 +0600

    refs #22 add validation work regex pattern

--- js/validate.js
+++ js/validate.js
@@ -33,19 +33,59 @@ var formData = new FormData(document.forms.request_form);
 var validate_input = function (){
 var formData = new FormData(document.forms.request_form);
 
+/*шаблоны полей*/
+  var name_pattern =/[A-Za-zА-Яа-я]{3,}/g;
+  var fone_pattern =/[A-Za-zА-Яа-я]{6,}/g;
+  var email_pattern =/[A-Za-zА-Яа-я]{3,}/g;
+  var text_pattern =/[A-Za-zА-Яа-я]{3,}/g;
+/*значения полей*/
+  var name = formData.get('name');
+  var fone = formData.get('fone');
+  var email = formData.get('email');
+  var text = formData.get('text');
+/*проверка на true false надо попробавать через масив???*/
+  var name_pattern=name_pattern.test(name);
+  var fone_pattern=fone_pattern.test(fone);
+  var email_pattern=email_pattern.test(email);
+  var text_pattern=text_pattern.test(text);
 
-var name_pattern ='/[A-Za-z]{3,}/';
-var fone_pattern ='';
-var email_pattern ='';
-var text_pattern ='';
 
 
-var name = formData.get('name');
-var fone = formData.get('fone');
-var email = formData.get('email');
-var text = formData.get('text');
+/*в качестве флага беру результат регулярки*/
+    /*валидация имени*/
+    if (name_pattern == true) {
+        /*continue;*/
+    } else {
+        /*менем цвет бордера*/
+    }
 
-  }
+    /*валидация телефона*/
+    if (fone_pattern == true) {
+        /*continue;*/
+    } else {
+        /*менем цвет бордера*/
+    }
+
+    /*валидация емаила*/
+    if (email_pattern == true) {
+        /*continue;*/
+    } else {
+        /*менем цвет бордера*/
+    }
+
+    /*валидация текста*/
+    if (text_pattern == true) {
+        /*continue;*/
+    } else {
+        /*менем цвет бордера*/
+    }
+
+   console.log(name_pattern, fone_pattern, email_pattern, text_pattern);
+/*
+если флаг не false то вызываем ajax
+иначе continue;
+*/
+
 }
 
 
